b31ed01f6e0e21a9248b29f1a70dc1af5c8e1433
Jake Wozniak <jwozniak@pf-mac-mini-12.pathfind.com>
2017-10-26 16:31:33 -0400

    initial commit

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+ReactDOM.render(
+    <div>
+        <h1>DEPECHE MODE IS A SWEET BAND</h1>
+    </div>,
+    document.getElementById('root'),
+);
