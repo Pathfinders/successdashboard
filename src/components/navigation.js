2890292f260b7b2736e06d0bb88dba9a6eaf8572
Jake Wozniak <jwozniak@Jake-Wozs-Mac-mini.local>
2017-11-01 09:52:28 -0400

    Added outside onclick handler

--- src/components/navigation.js
+++ src/components/navigation.js
@@ -2,18 +2,28 @@ import React, { Component } from 'react';
 import AppBar from 'material-ui/AppBar';
 import Drawer from 'material-ui/Drawer';
 import MenuItem from 'material-ui/MenuItem';
+import FontIcon from 'material-ui/FontIcon';
+import onClickOutside from 'react-onclickoutside'
 
 class Navigation extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            open: false
+            open: false,
         };
     }
     toggleDrawer() {
         this.setState({
             open: !this.state.open
         })
+    }
+    closeDrawer() {
+        this.setState({
+            open: false
+        })
+    }
+    handleClickOutside(evt) {
+        this.closeDrawer()
     }
     render() {
         return (<div>
@@ -26,4 +36,9 @@ class Navigation extends Component {
     }
 }
 
-export default Navigation;
+var clickOutsideConfig = {
+    excludeScrollbar: true
+}
+var EnhancedComponent = onClickOutside(Navigation, clickOutsideConfig);
+
+export default onClickOutside(Navigation);
